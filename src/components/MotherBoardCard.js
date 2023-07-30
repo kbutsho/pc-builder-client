ab3c25a5c1bd82ec2d840227645006f23bb62bd5
Kaushik Biswas <kbutsho@gmail.com>
2023-07-30 16:06:22 +0600

    motherboard card added

---
+++ src/components/MotherBoardCard.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import { AiFillStar } from "react-icons/ai";
+
+const MotherBoardCard = (params) => {
+    const { name, image, category, status, price, averageRating, id } = params.motherboard
+    return (
+        <div className='col-lg-2 col-12'>
+            <div className='card'>
+                <div className="text-center py-2">
+                    <Image src={image} alt="img" width={120} height={120} responsive={true} />
+                </div>
+                <h6 className='my-2'>{name}</h6>
+                <ul className='list-unstyled' style={{ fontSize: "15px" }}>
+                    <li><small>{category}</small></li>
+                    <li><small>price {price}</small></li>
+                    <li><small>{status}</small></li>
+                    <li><small>rating {averageRating}
+                    </small><AiFillStar color="red" style={{ fontSize: "14px", margin: "0 0 2px 3px" }} /></li>
+                </ul>
+                <Link href={`/category/mother-board/${id}`} className='btn btn-primary btn-sm w-100 fw-bold'>details</Link>
+            </div>
+        </div>
+    );
+};
+
+export default MotherBoardCard;
