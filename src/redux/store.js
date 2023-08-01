d7fee1a1e468de8291b980cd2ebc8112ea350312
Kaushik Biswas <kbutsho@gmail.com>
2023-08-02 05:01:55 +0600

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import componentReducer from "./componentSlice";
 
-// const preloadedState = typeof window !== "undefined" ? JSON.parse(localStorage.getItem("components")) : [];
+
 
 const store = configureStore({
     reducer: {
         components: componentReducer,
-    },
-    // preloadedState,
+    }
 });
 
-// store.subscribe(() => {
-//     const state = store.getState();
-//     localStorage.setItem("components", JSON.stringify(state));
-// });
+
 export default store;
