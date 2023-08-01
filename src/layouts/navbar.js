e5daae27efd0b9ca7774cd088bbfaf7056f62121
Kaushik Biswas <kbutsho@gmail.com>
2023-08-02 05:14:43 +0600

    update

--- src/layouts/navbar.js
+++ src/layouts/navbar.js
@@ -36,10 +36,10 @@ const Navbar = () => {
                                     <li><Link className="dropdown-item" href="/category/others">Others</Link></li>
                                 </ul>
                             </div>
-                            <Link className="btn btn-warning px-3 fw-bold pb-1 mx-3" href="/build">Build PC</Link>
+                            <Link className="btn btn-warning fw-bold pb-2 mx-3 mb-1 navbar-btn" href="/build">Build PC</Link>
                             {
                                 session?.user ?
-                                    <button className="btn btn-danger fw-bold text-uppercase" onClick={logout}>Logout</button> :
+                                    <button className="btn btn-danger fw-bold text-uppercase navbar-btn" onClick={logout}>Logout</button> :
                                     <Link className="nav-link text-white" href="/login">Login</Link>
                             }
 
