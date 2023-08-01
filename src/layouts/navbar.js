c6e456b8eaa49cb10aed76f861ff4fb7cc922197
Kaushik Biswas <kbutsho@gmail.com>
2023-08-02 05:26:05 +0600

    update

--- src/layouts/navbar.js
+++ src/layouts/navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
                         <div className="navbar-nav ms-auto text-uppercase fw-bold">
                             <Link className="nav-link text-white" href="/">Home</Link>
                             <div className="dropdown">
-                                <a className="nav-link text-white dropdown-toggle"
+                                <a className="nav-link text-white dropdown-toggle me-3"
                                     data-bs-toggle="dropdown"
                                     style={{ cursor: "pointer" }}
                                     aria-expanded="false">
@@ -36,10 +36,10 @@ const Navbar = () => {
                                     <li><Link className="dropdown-item" href="/category/others">Others</Link></li>
                                 </ul>
                             </div>
-                            <Link className="btn btn-warning fw-bold pb-2 mx-3 mb-1 navbar-btn" href="/build">Build PC</Link>
+                            <Link className="btn btn-warning fw-bold pb-2 mb-1" href="/build">Build PC</Link>
                             {
                                 session?.user ?
-                                    <button className="btn btn-danger fw-bold text-uppercase navbar-btn" onClick={logout}>Logout</button> :
+                                    <button className="btn btn-danger fw-bold text-uppercase pb-2 mb-1" onClick={logout}>Logout</button> :
                                     <Link className="nav-link text-white" href="/login">Login</Link>
                             }
 
